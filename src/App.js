ac99508d97997bba980e4d9c74713c6bc8be428c
rahidaliyev <rahidaliyev18@gmail.com>
2022-07-07 21:53:46 +0400

    makeorder done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
+import MakeOrder from "./components/MakeOrder/MakeOrder";
 import OrderForm from "./components/OrderForm/OrderForm";
 
 function App() {
   return (
     <div>
-<OrderForm/>
+      <MakeOrder/>
 
     </div>
   );
--- src/components/MakeOrder/MakeOrder.jsx
+++ src/components/MakeOrder/MakeOrder.jsx
@@ -1,11 +1,33 @@
 import React from 'react'
+import '../../styles/MakeOrder.css'
 
 export default function MakeOrder() {
   return (
-    <div>
+    <div className='container'>
+<h1>Aşağıdakı listdən məhsul seçimini edin</h1>
 
+<form className='mainform'>
 
-        
+<label >Məhsul adı</label>
+<select className='firstselect'>
+  <option ></option>
+  <option label="Çörəkdə Dönər"></option>
+  <option label="Kabab"></option>
+  <option label="Dolma"></option>
+  <option label="Lülə"></option>
+</select>
+</form>
+
+<div className='lasttwo'>
+<div className='secondcontainer'>
+<label>Miqdar</label>
+<input className='amount'/>
+</div>
+<div className='price'>
+<babel>Qiyməti</babel>
+<input/>
+</div>
+</div>
     </div>
   )
 }

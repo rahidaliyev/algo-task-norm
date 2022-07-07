e71f8de9924e39187e1f5d33d55e199b94a17023
rahidaliyev <rahidaliyev18@gmail.com>
2022-07-07 19:33:13 +0400

    form component created

---
+++ src/components/MakeOrderList/MakeOrderList.jsx
@@ -0,0 +1,65 @@
+import React from 'react'
+
+export default function MakeOrderList() {
+  return (
+    <div>
+<table>
+  <thead>
+    <tr>
+      <th>Sıra Nömrəsi</th>
+      <th>Masa</th>
+      <th>Xidmətçi</th>
+      <th>Status</th>
+      <th>Məbləğ</th>
+      <th>Sonlanma Tarixi</th>
+      <th>Ətraflı</th>
+       </tr>
+    </thead>
+
+    <tbody>
+    <tr>
+      <td>1</td>
+      <td>m9</td>
+      <td>Murtuz</td>
+      <td>Sonlanmayıb</td>
+      <td>78.3AZN</td>
+      <td>---</td>
+      <td><button>bax</button></td>
+      
+    </tr>
+    <tr>
+      <td>2</td>
+      <td>m4</td>
+      <td>Murtuz</td>
+      <td>Sonlanmayıb</td>
+      <td>78.3AZN</td>
+      <td>---</td>
+      <td><button>bax</button></td>
+      
+    </tr>
+    <tr>
+      <td>3</td>
+      <td>m3</td>
+      <td>Murtuz</td>
+      <td>Sonlanmayıb</td>
+      <td>78.3AZN</td>
+      <td>---</td>
+      <td><button>bax</button></td>
+      
+    </tr>
+    <tr >
+      <td>4</td>
+      <td>m6</td>
+      <td>Murtuz</td>
+      <td>Sonlanmayıb</td>
+      <td>78.3AZN</td>
+      <td>---</td>
+      <td><button>bax</button></td>
+      
+    </tr>
+  </tbody>
+</table>
+
+    </div>
+  )
+}
---
+++ src/components/OrderForm/OrderForm.jsx
@@ -0,0 +1,66 @@
+import React from 'react'
+import '../../styles/OrderForm.css'
+
+export default function OrderForm() {
+  return (
+    <div>
+<table>
+  <thead>
+    <tr>
+      <th>Sıra Nömrəsi</th>
+      <th>Masa</th>
+      <th>Xidmətçi</th>
+      <th>Status</th>
+      <th>Məbləğ</th>
+      <th>Sonlanma Tarixi</th>
+      <th>Ətraflı</th>
+       </tr>
+    </thead>
+
+    <tbody>
+    <tr>
+      <td>1</td>
+      <td>m9</td>
+      <td>Murtuz</td>
+      <td>Sonlanmayıb</td>
+      <td>78.3AZN</td>
+      <td>---</td>
+      <td><button>bax</button></td>
+      
+    </tr>
+    <tr>
+      <td>2</td>
+      <td>m4</td>
+      <td>Murtuz</td>
+      <td>Sonlanmayıb</td>
+      <td>78.3AZN</td>
+      <td>---</td>
+      <td><button>bax</button></td>
+      
+    </tr>
+    <tr>
+      <td>3</td>
+      <td>m3</td>
+      <td>Murtuz</td>
+      <td>Sonlanmayıb</td>
+      <td>78.3AZN</td>
+      <td>---</td>
+      <td><button>bax</button></td>
+      
+    </tr>
+    <tr >
+      <td>4</td>
+      <td>m6</td>
+      <td>Murtuz</td>
+      <td>Sonlanmayıb</td>
+      <td>78.3AZN</td>
+      <td>---</td>
+      <td><button>bax</button></td>
+      
+    </tr>
+  </tbody>
+</table>
+
+    </div>
+  )
+}

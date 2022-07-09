0cf205fc49f5ba5cba3f65cf4477d127e962d7a3
rahidaliyev <rahidaliyev18@gmail.com>
2022-07-09 19:47:50 +0400

    api added

---
+++ client/src/api.js
@@ -0,0 +1,16 @@
+[
+
+    { "id": 1, "name": "sugar", "quantity": 30, "price": 30 },
+    { "id": 2, "name": "tea", "quantity": 31, "price": 20 },
+    { "id": 3, "name": "oil", "quantity": 32, "price": 20 },
+    { "id": 4, "name": "bread", "quantity": 33, "price": 20 },
+    { "id": 5, "name": "pen", "quantity": 34, "price": 20 },
+    { "id": 6, "name": "apple", "quantity": 35, "price": 20 },
+    { "id": 7, "name": "lemon", "quantity": 36, "price": 20 },
+    { "id": 8, "name": "macaroni", "quantity": 37, "price": 20 },
+    { "id": 9, "name": "tomato", "quantity": 38, "price": 20 },
+    { "id": 11, "name": "cucumber", "quantity": 39, "price": 20 },
+    { "id": 12, "name": "zomato", "quantity": 39, "price": 20 },
+    { "id": 13, "name": "watermalon", "quantity": 39, "price": 20 },
+    { "id": 14, "name": "onion", "quantity": 3, "price": 20 }
+]
